8e4b7e7895101de85a858e1308ad5d5502f4d2f9
Smaine Milianni <smaine.milianni@gmail.com>
2019-09-22 10:18:36 +0100

    add maps !

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Diaporama from './Diaporama';
+import MapContainer from './Maps/MapContainer';
 
 class Layout extends React.Component {
     render() {
         return <div>
         <Diaporama />
+        <MapContainer />
         </div>
     }
 }
